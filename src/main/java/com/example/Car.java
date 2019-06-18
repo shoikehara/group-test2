3f6666aad529a7fae3f6a86d20914d023a2803ce
risa.nazato <risa.nazato@rakus.co.jp>
2019-06-18 14:46:07 +0900

    Carクラス追記

--- src/main/java/com/example/Car.java
+++ src/main/java/com/example/Car.java
@@ -1,5 +1,27 @@
 package com.example;
 
 public class Car {
-
+	private String carname;
+	private String carcolor;
+	private int gas;
+	public String getCarname() {
+		return carname;
+	}
+	public void setCarname(String carname) {
+		this.carname = carname;
+	}
+	public String getCarcolor() {
+		return carcolor;
+	}
+	public void setCarcolor(String carcolor) {
+		this.carcolor = carcolor;
+	}
+	public int getGas() {
+		return gas;
+	}
+	public void setGas(int gas) {
+		this.gas = gas;
+	}
+	
+	
 }
