8d3819353e6a685dd4cff012d6be3ab27c0f291a
sho.ikehara <sho.ikehara@rakus.co.jp>
2019-06-18 14:37:47 +0900

    Carクラスを作成。

---
+++ src/main/java/com/example/GroupTest2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupTest2Application.class, args);
+	}
+
+}
