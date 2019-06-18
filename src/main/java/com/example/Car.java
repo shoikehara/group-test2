0e2fd5a1d1adaab5018fd02dc27dd293a005b121
sho.ikehara <sho.ikehara@rakus.co.jp>
2019-06-18 14:48:06 +0900

    Carクラス修正

--- src/main/java/com/example/Car.java
+++ src/main/java/com/example/Car.java
@@ -1,5 +1,19 @@
 package com.example;
 
 public class Car {
-
+	private int speed;
+	private String bodyColor;
+	
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public String getBodyColor() {
+		return bodyColor;
+	}
+	public void setBodyColor(String bodyColor) {
+		this.bodyColor = bodyColor;
+	}
 }
